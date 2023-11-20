ff4457cd702766a5ceeffe12401e72ffc9fcc96f
CLaudioGl14 <claudiosg9@hotmail.com>
2023-11-20 13:25:13 -0500

    first commit

---
+++ src/main/java/ProyectoDaw/PromoAuth/PromoAuthApplication.java
@@ -0,0 +1,13 @@
+package ProyectoDaw.PromoAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoDaw/PromoAuth/PromoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoDaw.PromoAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
